5364d4d9d12c0d0936353b30c18d9d0cf0b4e60d
kadunapikin <ibakoshi27@gmail.com>
2023-03-19 13:11:17 +0100

    write a playerChoice function to take either rock, paper or scissors as input

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,3 +3,8 @@ const choices = ["rock", "paper", "scissors"];
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playerChoice() {
+    let input = prompt("Type Rock, Paper or Scissors");
+    input = input.toLowerCase();
+}
