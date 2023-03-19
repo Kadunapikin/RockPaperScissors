08e53d3b915bda8bfa11fff0dbbaa7bad5e91245
kadunapikin <ibakoshi27@gmail.com>
2023-03-19 13:39:54 +0100

    write a function to check and return the winner

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -8,3 +8,16 @@ function playerChoice() {
     let input = prompt("Type Rock, Paper or Scissors");
     input = input.toLowerCase();
 }
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return "it\'s a tie";
+    } else if (
+        (choiceP === "rock" && choiceC == "scissors") ||
+        (choiceP === "paper" && choiceC == "rock") ||
+        (choiceP === "scissors" && choiceC == "paper")) {
+    return "plare wins";
+    } else {
+        return "computer wins"
+    }
+}
