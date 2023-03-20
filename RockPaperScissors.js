0d96b6abf69f23e9c735536336a8ec60f8bb6533
kadunapikin <ibakoshi27@gmail.com>
2023-03-20 09:13:37 +0100

    write a code to vaidate and check playerChoice

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,19 +1,42 @@
 const choices = ["rock", "paper", "scissors"];
 
+function game() {
+    playRound();
+}
+
 function playRound() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
     const winner = checkWinner(playerSelection, computerSelection);
+    console.log(playerSelection);
+    console.log(computerSelection);
+    console.log(winner);
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playerChoice() {
+function playerChoice(){
     let input = prompt("Type Rock, Paper or Scissors");
+    while (input == null){
+      input = prompt("Type Rock, Paper or Scissors");
+    }
     input = input.toLowerCase();
-}
+    let check = validateInput(input);
+    while(check == false){
+      input = prompt(
+       "type Rock, Paper or Scissors. Spelling needs to be exact, but capitalization doesnt matter"
+      );
+      while (input == null){
+        input = prompt("type Rock, Paper or Scissors");
+     }
+     input = input.toLowerCase();
+     check = validateInput(input);
+   } 
+   return input; 
+  }
+
 
 function validateInput(choice) {
 return choices.includes(choice);
@@ -21,13 +44,15 @@ return choices.includes(choice);
 
 function checkWinner(choiceP, choiceC) {
     if (choiceP === choiceC) {
-        return "it\'s a tie";
+        return "it's a tie";
     } else if (
         (choiceP === "rock" && choiceC == "scissors") ||
         (choiceP === "paper" && choiceC == "rock") ||
         (choiceP === "scissors" && choiceC == "paper")) {
-    return "plare wins";
+    return "player wins";
     } else {
         return "computer wins"
     }
 }
+
+game();
