0383f9a68378e0c84baf37fb54814ee85cf1a35c
kadunapikin <ibakoshi27@gmail.com>
2023-03-20 13:41:18 +0100

    add a scissors button

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,79 +1,79 @@
-const choices = ["rock", "paper", "scissors"];
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
-const winner = checkWinner(playerSelection, computerSelection);
-let playerScore = 0;
-let computerScore = 0;
-let roundCount = 1;
+// const choices = ["rock", "paper", "scissors"];
+// const playerSelection = playerChoice();
+// const computerSelection = computerChoice();
+// const winner = checkWinner(playerSelection, computerSelection);
+// let playerScore = 0;
+// let computerScore = 0;
+// let roundCount = 1;
 
 
-function game() {
-    playRound();
-}
+// function game() {
+//     playRound();
+// }
 
-function playRound() {
-    while (roundCount <= 5) {
-        console.log(`playerChoice ${playerSelection}`);
-        console.log(`computerChoice ${computerSelection}`);    
-        console.log(`Round ${roundCount}:`);
-        console.log(winner);
-        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
-        roundCount ++;
-    }
-    if (playerScore > computerScore) {
-        console.log(`You won the game! Final score: ${playerScore}-${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`You lost the game! Final score: ${computerScore}-${playerScore}`);
-    } else {
-        console.log(`It\'s a draw! Final score: ${playerScore}-${computerScore}`);
-    }
-}
-
-
-function computerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function playerChoice(){
-    let input = prompt("Type Rock, Paper or Scissors");
-    while (input == null){
-      input = prompt("Type Rock, Paper or Scissors");
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    while(check == false){
-      input = prompt(
-       "type Rock, Paper or Scissors. Spelling needs to be exact, but capitalization doesnt matter"
-      );
-      while (input == null){
-        input = prompt("type Rock, Paper or Scissors");
-     }
-     input = input.toLowerCase();
-     check = validateInput(input);
-   } 
-   return input; 
-  }
+// function playRound() {
+//     while (roundCount <= 5) {
+//         console.log(`playerChoice ${playerSelection}`);
+//         console.log(`computerChoice ${computerSelection}`);    
+//         console.log(`Round ${roundCount}:`);
+//         console.log(winner);
+//         console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+//         roundCount ++;
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`You won the game! Final score: ${playerScore}-${computerScore}`);
+//     } else if (computerScore > playerScore) {
+//         console.log(`You lost the game! Final score: ${computerScore}-${playerScore}`);
+//     } else {
+//         console.log(`It\'s a draw! Final score: ${playerScore}-${computerScore}`);
+//     }
+// }
 
 
-function validateInput(choice) {
-return choices.includes(choice);
-}
+// function computerChoice() {
+//     return choices[Math.floor(Math.random() * choices.length)];
+// }
 
-function checkWinner(userChoice, computerChoice) {
-    let roundWinner;
-  if (userChoice === computerChoice) {
-    roundWinner = "tie";
-  } else if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
-    roundWinner = "user";
-    userScore++;
-  } else {
-    roundWinner = "computer";
-    computerScore++;
-  }
-}
+// function playerChoice(){
+//     let input = prompt("Type Rock, Paper or Scissors");
+//     while (input == null){
+//       input = prompt("Type Rock, Paper or Scissors");
+//     }
+//     input = input.toLowerCase();
+//     let check = validateInput(input);
+//     while(check == false){
+//       input = prompt(
+//        "type Rock, Paper or Scissors. Spelling needs to be exact, but capitalization doesnt matter"
+//       );
+//       while (input == null){
+//         input = prompt("type Rock, Paper or Scissors");
+//      }
+//      input = input.toLowerCase();
+//      check = validateInput(input);
+//    } 
+//    return input; 
+//   }
 
-game();
+
+// function validateInput(choice) {
+// return choices.includes(choice);
+// }
+
+// function checkWinner(userChoice, computerChoice) {
+//     let roundWinner;
+//   if (userChoice === computerChoice) {
+//     roundWinner = "tie";
+//   } else if (
+//     (userChoice === "rock" && computerChoice === "scissors") ||
+//     (userChoice === "paper" && computerChoice === "rock") ||
+//     (userChoice === "scissors" && computerChoice === "paper")
+//   ) {
+//     roundWinner = "user";
+//     userScore++;
+//   } else {
+//     roundWinner = "computer";
+//     computerScore++;
+//   }
+// }
+
+// game();
