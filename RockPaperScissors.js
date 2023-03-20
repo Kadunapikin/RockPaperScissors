769edb720548cf8fa0a53385042cc475bcf6aa07
kadunapikin <ibakoshi27@gmail.com>
2023-03-20 14:13:16 +0100

    declare all buttons as a variable

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,7 @@
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const playerChoiceDisplay = document.getElementById('player-choice');
 const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
 
 
 const choices = ["rock", "paper", "scissors"];
